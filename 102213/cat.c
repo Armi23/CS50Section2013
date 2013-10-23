b05b4c8e61ca22255ca4c5a9e420b7e634bc2cbf
Armi <armaghan23@gmail.com>
2013-10-22 23:27:44 -0400

    cat

---
+++ 102213/cat.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    // check if enough input arguments were given
+    if (argc < 2)
+    {
+        printf("Usage: cat file1 file2 ..\n");
+        return 1;
+    }
+
+    // open output file.
+    FILE* output = fopen("output.txt","w");
+    
+    if (output == NULL)
+    {
+        printf("Could not open output\n");
+        return 1;
+    }
+
+
+    // open each file given on the command line, one by one
+    for(int i = 1; i < argc; i++)
+    {
+        FILE* file = fopen(argv[i],"r");
+
+        // check if they could be opened
+        if (file == NULL)
+        {
+            printf("couldn't open %s\n",argv[i]);
+            return 1;
+        }
+        
+        // read in from file and put character you read into output. 
+        for (int c = fgetc(file); c != EOF; c = fgetc(file))
+        {
+            //putc(c,output);
+            fwrite(&c,sizeof(char),1,output);
+        }
+        
+        fclose(file);
+    }
+    
+    return 0;
+
+
+
+}
